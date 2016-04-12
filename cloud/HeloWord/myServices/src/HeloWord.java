5eba74e9f7aa5d74fd5dcaf39d7e953d007cf8da
Carlos Alberto Fernandes Neves Filho <cafnf70@gmail.com>
2016-04-12 16:13:41 -0300

    HeloWord

---
+++ cloud/HeloWord/myServices/src/HeloWord.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Beto
+ */
+public class HeloWord {
+    public String plus_a(String in) {
+        return in + "a";
+    }
+}
